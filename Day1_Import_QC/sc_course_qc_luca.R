1f285e0837e75adfa6bf77d6a61a5d03ba7604a7
LugliLab <sinnamone@gmail.com>
2023-09-11 22:32:43 +0200

    update Day1

--- Day1_Import_QC/sc_course_qc_luca.R
+++ Day1_Import_QC/sc_course_qc_luca.R
@@ -8,30 +8,33 @@ options(Seurat.object.assay.version = 'v5')
 
 #####load and create Seurat object from 10x cellranger count output directory
 
-sample_1 <- Read10X("corso_sc/Patient_01/raw_feature_bc_matrix/")
-sample_2 <- Read10X("corso_sc/Patient_02/raw_feature_bc_matrix/")
-sample_3 <- Read10X("corso_sc/Patient_03/raw_feature_bc_matrix/")
+  sample_1 <- Read10X("Patient_01/raw_feature_bc_matrix/")
+sample_2 <- Read10X("Patient_02/raw_feature_bc_matrix/")
+#sample_3 <- Read10X("corso_sc/Patient_03/raw_feature_bc_matrix/")
 
 #if you have a metadata table by cell barcode as row and feature as column can provide by meta.data option
 sample_1 <- CreateSeuratObject(counts = sample_1)
 sample_2 <- CreateSeuratObject(counts = sample_2)
-sample_3 <- CreateSeuratObject(counts = sample_3)
+#sample_3 <- CreateSeuratObject(counts = sample_3)
 
 
 #####define Sample variable for each sample
-sample_1$Sample <- "Patient 1"
-sample_2$Sample <- "Patient 2"
-sample_3$Sample <- "Patient 3"
+sample_1$Sample <- "Patient_1"
+sample_2$Sample <- "Patient_2"
+
+#sample_3$Sample <- "Patient 3"
 
 
 #####in droplet based single-cell analysis we obtain many empty droplet. 
 ####To remove them we build a curve that shows the log-count of umi associated with each ranked barcode 
 
-seuratList<-list(sample_1,sample_2,sample_3)
+seuratList<-list(sample_1,sample_2)
 
 seuratList<- lapply(seuratList,function(x){CalculateBarcodeInflections(x,threshold.high = 10000,threshold.low = 1000)})
 
+pdf("curve_barcode.pdf")
 BarcodeInflectionsPlot(seuratList[[1]]) ##dotted line is the inflection point computed in sample_1. 
+dev.off()
 ###We should zoom to appreciate the inversion of the curve
 
 seuratList<- lapply(seuratList,SubsetByBarcodeInflections)
@@ -43,12 +46,14 @@ seuratList<- lapply(seuratList, function(x){PercentageFeatureSet(x, pattern = "^
 seuratList<- lapply(seuratList,function(x){PercentageFeatureSet(x, pattern = "^MT-", col.name = "percent.mito")})
 
 #####now let's display some of the most used metrics for qc
-
+pdf("ViolinPlotRibo.pdf")
 VlnPlot(seuratList[[1]], features = c("percent.ribo","percent.mito","nCount_RNA","nFeature_RNA"))
+dev.off()
 
+pdf("ViolinPlotRibo_ptz2.pdf")
 VlnPlot(seuratList[[2]], features = c("percent.ribo","percent.mito","nCount_RNA","nFeature_RNA"))
-
-VlnPlot(seuratList[[3]], features = c("percent.ribo","percent.mito","nCount_RNA","nFeature_RNA"))
+dev.off()
+#VlnPlot(seuratList[[3]], features = c("percent.ribo","percent.mito","nCount_RNA","nFeature_RNA"))
 
 
 ####the tresholds for cutting low-quality cells can be chosen arbitrarily or by trying to identify which cells are outliers for each 
@@ -63,17 +68,21 @@ seuratList<- lapply(seuratList,function(x){subset(x, subset = percent.mito <= 14
 
 
 #####now we can visualize the cleaned dataset
-
+#####now let's display some of the most used metrics for qc
+pdf("ViolinPlot_postQC_ptz1.pdf")
 VlnPlot(seuratList[[1]], features = c("percent.ribo","percent.mito","nCount_RNA","nFeature_RNA"))
-
+dev.off()
+pdf("ViolinPlot_postQC_ptz2.pdf")
 VlnPlot(seuratList[[2]], features = c("percent.ribo","percent.mito","nCount_RNA","nFeature_RNA"))
+dev.off()
 
-VlnPlot(seuratList[[3]], features = c("percent.ribo","percent.mito","nCount_RNA","nFeature_RNA"))
+#VlnPlot(seuratList[[3]], features = c("percent.ribo","percent.mito","nCount_RNA","nFeature_RNA"))
 
 
 ###remove low expressed genes which could affect the normalization step. We filtered out all genes which are expressed
 ### less than 1% of all cells
 
+### Version 5
 for(i in 1:length(seuratList)){
   counts<-LayerData(seuratList[[i]],layer = "counts")
   cell_percent<- rowMeans(counts>0)*100
@@ -81,14 +90,24 @@ for(i in 1:length(seuratList)){
   seuratList[[i]]<-seuratList[[i]][expressed_genes,]
 }
 
+### Version 4
+for(i in 1:length(seuratList)){
+  counts<-GetAssayData(seuratList[[i]],slot = "counts")
+  cell_percent<- rowMeans(counts>0)*100
+  expressed_genes<-names(cell_percent[cell_percent>1])
+  seuratList[[i]]<-seuratList[[i]][expressed_genes,]
+}
+
+
+
 
 #####merge all filtered sample into one Seurat object  
 
-seuratObj <- merge(seuratList[[1]], c(seuratList[[2]],seuratList[[3]]), add.cell.ids=c("sample_1","sample_2","sample_3"))
+seuratObj <- merge(seuratList[[1]], c(seuratList[[2]]), add.cell.ids=c("sample_1","sample_2"))
 
 ##we can remove the 3 objects no longer needed from the environment
 
-remove(sample_1,sample_2,sample_3,seuratList)
+remove(sample_1,sample_2,seuratList)
 gc()
 
 #####let's look closely at some characteristics of the created object
@@ -132,4 +151,17 @@ seuratObj<-NormalizeData(seuratObj)
 ##now we can see that the data layers containing the normalized counts has been added
 Layers(seuratObj)
 
-seuratObj@assays$RNA$data.1[1:10,1:5]
+seuratObj@assays$RNA@data[1:10,1:5]
+
+##to save an object
+saveRDS(seuratObj,"QC_seurat_object.rds")
+#to reload the object
+
+seuratObj<- readRDS("seurat_object.rds")
+
+##to save the entire envinroment
+save.image("seurat_analysis.Rda")
+
+#to reload the env
+load("seurat_analysis.Rda")
+
